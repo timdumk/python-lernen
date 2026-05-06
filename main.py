aed54dfd4c250b3423715cc51f053ab68980f1d4
timdumk <dumkowskitim@gmail.com>
2026-05-06 12:01:25 +0200

    variables, input, if-elif-else

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Hello World")
+age = int(input("How old are you? "))
+if age >= 18:
+    print("You are an adult.")
+elif age >= 13:
+    print("You are a teenager.")
+else:
+    print("You are a child.")
+
